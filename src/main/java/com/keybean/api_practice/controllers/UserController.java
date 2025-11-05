4dd5b17a04cd06da1e7d659426299d9adce05582
keybean013 <backtik08@gmail.com>
2025-11-06 00:42:53 +0800

    2nd testing push

--- src/main/java/com/keybean/api_practice/controllers/UserController.java
+++ src/main/java/com/keybean/api_practice/controllers/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
     private String test = "test";
+    private String test2 = "test2";
 
     @Autowired
     private UserService userService;
