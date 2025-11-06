4b7ba5253fa496d42306ac44c7f579469cb6a772
keybean013 <backtik08@gmail.com>
2025-11-07 03:48:25 +0800

    [Back to zero]

--- src/main/java/com/keybean/creating_api2/service/RoleService.java
+++
@@ -1,9 +0,0 @@
-package com.keybean.creating_api2.service;
-
-import com.keybean.creating_api2.dto.RoleDTO;
-
-import java.util.List;
-
-public interface RoleService {
-
-}
--- src/main/java/com/keybean/creating_api2/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.keybean.creating_api2.service;
-
-import com.keybean.creating_api2.dto.UserDTO;
-
-import java.util.List;
-
-public interface UserService {
-
-}
