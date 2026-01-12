cc6bcd8301907af2df2f1acc0cc5c205d47cd2b7
keybean013 <backtik08@gmail.com>
2026-01-13 00:14:53 +0800

    [CHANGES][DELETE] - starting role CRUD feature

--- src/main/java/com/keybean/creating_api2/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.keybean.creating_api2.repository;
-
-import com.keybean.creating_api2.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-
-}
